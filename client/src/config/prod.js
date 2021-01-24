ca90922d47d12d9fcab83eea1cf2ab5bb2768c43
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-01-24 02:26:11 -0500

    prod debugging

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY_ONE,
     cookieKeyTwo: process.env.COOKIE_KEY_TWO,
-    passphrase: process.env.PASSPHRASE
+    passphrase: '123'
 }
