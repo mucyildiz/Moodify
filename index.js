593f8d9240bbd964d32c102437c25cf362d67ea2
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-07-16 18:38:22 -0400

    Intialize routes

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const express = require('express');
 const keys = require('./config/keys');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
+const session = require('express-session');
 require('./services/passport');
 
 const app = express();
 
 app.use(cookieSession({
     name: 'session',
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in ms
     keys: [keys.cookieKey, keys.cookieKeyTwo]
 }))
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/userRoutes')(app);
+require('./routes/spotifyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
